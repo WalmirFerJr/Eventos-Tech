fb42ebc606dbd457e37fa26028a9154609a6d023
walmirferjr <walmir.jr.2004@usp.br>
2026-02-15 04:14:45 -0300

    feat: Adicionando Repositorys

---
+++ src/main/java/com/eventostech/api/repositories/AddressRepository.java
@@ -0,0 +1,11 @@
+package com.eventostech.api.repositories;
+
+import com.eventostech.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+
+
+}
