b28bb232bf384fe68e6d5dc746f0df6d77149b39
walmirferjr <walmir.jr.2004@usp.br>
2026-02-14 23:18:38 -0300

    feat: criado entidade Address

--- src/main/java/com/eventostech/api/domain/coupom/coupom.java
+++ src/main/java/com/eventostech/api/domain/coupom/coupom.java
@@ -1,4 +1,38 @@
 package com.eventostech.api.domain.coupom;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "event-coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class coupom {
+
+    @Id
+    @GeneratedValue
+
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+
+
+
+
 }
