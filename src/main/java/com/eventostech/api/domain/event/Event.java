2c1a231a60f00906510a2cf60eb9032266c5bcdf
walmirferjr <walmir.jr.2004@usp.br>
2026-02-19 04:30:21 -0300

    feat: Incluindo EventRequest e EventService

--- src/main/java/com/eventostech/api/domain/event/Event.java
+++ src/main/java/com/eventostech/api/domain/event/Event.java
@@ -12,26 +12,21 @@ import lombok.Setter;
 import java.util.Date;
 import java.util.UUID;
 
-@Table(name = "evemt")
+@Table(name = "event")
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Event {
     @Id
     @GeneratedValue
-
     private UUID id;
 
     private String title;
-
     private String description;
-
+    private String imgUrl;
     private String eventUrl;
-
     private Boolean remote;
-
     private Date date;
 }
