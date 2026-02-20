b5b4442e35eccebce4971fc9608796f8f8b86280
walmirferjr <walmir.jr.2004@usp.br>
2026-02-19 23:15:26 -0300

    feat: Configurando S3 Bucket

---
+++ src/main/java/com/eventostech/api/service/EventService.java
@@ -0,0 +1,67 @@
+package com.eventostech.api.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.eventostech.api.domain.event.Event;
+import com.eventostech.api.domain.event.EventRequestDTO;
+import com.sun.jdi.request.EventRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+
+public class EventService {
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public Event createEvent(EventRequestDTO data){
+        String imgUrl = null;
+
+        if(data.image() != null){
+           imgUrl = this.uploadImg(data.image());
+        }
+
+        Event newEvent = new Event();
+        newEvent.setTitle(data.title());
+        newEvent.setDescription(data.description());
+        newEvent.setEventUrl(data.eventUrl());
+        newEvent.setDate(new Date(data.date()));
+        newEvent.setImgUrl(imgUrl);
+
+        return newEvent;
+    }
+
+    private String uploadImg(MultipartFile multipartFile){
+        String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try{
+            File file = this.convertMultipartToFile(multipartFile);
+            s3Client.putObject(bucketName, filename, file);
+            file.delete();
+            return s3Client.getUrl(bucketName, filename).toString();
+        } catch (Exception e) {
+            System.out.print("erro ao subir arquivo");
+            return null;
+        }
+    }
+
+    private File convertMultipartToFile(MultipartFile multipartFile) throws IOException {
+        File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return convFile;
+    }
+
+}
